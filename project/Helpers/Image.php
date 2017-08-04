6b24d17d5a5c4717ce960332ead5ae5f32deccad
V1acl <v1acl.zakupka@gmail.com>
2017-08-04 19:38:15 +0300

    костыль если нету imagick

--- project/Helpers/Image.php
+++ project/Helpers/Image.php
@@ -17,15 +17,40 @@ class Image
 {
 
     /**
+     * TODO: Реализовать с проверкой движка, сделать 2 класса с одним интерфейсом с возможностью ресайзить
+     *
      * @param string $filePath
-     * @param int $width
-     * @param int $height
+     * @param int $maxWidth
+     * @param int $maxHeight
+     * @internal param int $width
+     * @internal param int $height
      */
-    public static function resize(string $filePath, int $width, int $height) {
-        $imagick = new \Imagick($filePath);
-        if ($imagick->getImageWidth() > $width || $imagick->getImageHeight() > $height) {
-            $imagick->scaleImage($width, $height, 1);
-            $imagick->writeImage();
+    public static function resize(string $filePath, int $maxWidth, int $maxHeight) {
+        if (class_exists('Imagick')) {
+            $imagick = new \Imagick($filePath);
+            if ($imagick->getImageWidth() > $maxWidth || $imagick->getImageHeight() > $maxHeight) {
+                $imagick->scaleImage($maxWidth, $maxHeight, 1);
+                $imagick->writeImage();
+            }
+        } else {
+            list($imageWidth, $imageHeight) = getimagesize($filePath);
+            if ($imageWidth > $maxHeight && $imageHeight > $maxHeight) {
+                $widthRatio = $imageWidth / $maxWidth;
+                $heightRation = $imageHeight / $maxHeight;
+                if ($widthRatio > $heightRation) {
+                    $width = $imageWidth / $widthRatio;
+                    $height = $imageHeight / $widthRatio;
+                } else {
+                    $width = $imageWidth / $heightRation;
+                    $height = $imageHeight / $heightRation;
+                }
+                $src = imagecreatefromstring(file_get_contents($filePath));
+                $dst = imagecreatetruecolor($width, $height);
+                imagecopyresampled($dst, $src, 0, 0, 0, 0, $width, $height, $imageWidth, $imageHeight);
+                imagedestroy($src);
+                imagepng($dst, $filePath);
+                imagedestroy($dst);
+            }
         }
     }
 
